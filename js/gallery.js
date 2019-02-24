03265f51bd0073a1651645139009d3e7009a6b74
Pentarock <pentarocktz@gmail.com>
2019-02-25 02:21:19 +0300

    w

---
+++ js/gallery.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+     
+      $("#owl-demo").owlCarousel({
+     
+          autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3]
+     
+      });
+     
+    });
+
+
+$(function() {
+var selectedClass = "";
+$(".filter").click(function(){
+selectedClass = $(this).attr("data-rel");
+$("#gallery").fadeTo(100, 0.1);
+$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+setTimeout(function() {
+$("."+selectedClass).fadeIn().addClass('animation');
+$("#gallery").fadeTo(300, 1);
+}, 300);
+});
+});
